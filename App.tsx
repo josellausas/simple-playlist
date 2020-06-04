f6b5119e021f45ca72d354350a92fbbc52ff64cf
zunware <jose@josellausas.com>
2020-06-04 16:25:13 -0500

    Testing builds

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Simple Playlist App
  *
  * Generated with the TypeScript template
  * https://github.com/react-native-community/react-native-template-typescript
